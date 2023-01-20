0c07f7b65627166a367dbac11a3dbfcdbaf8d29f
josevqzmdz <notanothergenericaddress@gmail.com>
2023-01-20 13:44:42 -0600

    changed static rendering for adapter-node

--- src/routes/+layout.js
+++
@@ -1,6 +0,0 @@
-/*  
-https://kit.svelte.dev/docs/adapter-static
-add the prerender option to your root layout:
-*/
-
-export const prerender = false;
--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,12 @@
 
 // https://rodneylab.com/sveltekit-node-app-deploy/
-// https://kit.svelte.dev/docs/adapter-static
+// https://kit.svelte.dev/docs/adapter-node
 
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-node';
  
 export default {
   kit: {
-    adapter: adapter({
-      // default options are shown. On some platforms
-      // these options are set automatically — see below
-      pages: 'build',
-      assets: 'build',
-      fallback: null,
-      precompress: false,
-      strict: true
-    })
+    adapter: adapter()
   }
 };
 /*

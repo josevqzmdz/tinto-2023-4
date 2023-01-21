eaac535203e33ce3ac3a7325fb572e849073345d
josevqzmdz <notanothergenericaddress@gmail.com>
2023-01-21 06:27:25 -0600

    revert ./ thingy

--- src/routes/image-slider.svelte
+++ src/routes/image-slider.svelte
@@ -6,50 +6,50 @@
         integrity="sha256-nQLuAZGRRcILA+6dMBOvcRh5Pe310sBpanc6+QBmyVM="
         crossorigin="anonymous">
     </script>
-    <link type="text/css" rel="stylesheet" href="./css/lightslider.css" />                  
-    <script src="./js/lightslider.js"></script>        
+    <link type="text/css" rel="stylesheet" href="../../src/routes/css/lightslider.css" />                  
+    <script src="../../src/routes/js/lightslider.js"></script>        
 </head>
 
 <body>
 <ul id="lightSlider">
     <li>
-        <img src="./images/images/tinto.jpg">
+        <img src="../../src/routes/images/images/tinto.jpg">
     </li>
     <li>
-        <img src="./images/images/pic1tinto.jpg">
+        <img src="../../src/routes/images/images/pic1tinto.jpg">
     </li> 
     <li>
-        <img src="./images/images/pic2tinto.jpg">
+        <img src="../../src/routes/images/images/pic2tinto.jpg">
     </li>    
     <li>
-        <img src="./images/images/pic3tinto.jpg">
+        <img src="../../src/routes/images/images/pic3tinto.jpg">
     </li>     
     <li>
-        <img src="./images/images/pic4tinto.jpg">
+        <img src="../../src/routes/images/images/pic4tinto.jpg">
     </li>      
     <li>
-        <img src="./images/images/pic5tinto.jpg">
+        <img src="../../src/routes/images/images/pic5tinto.jpg">
     </li> 
     <li>
-        <img src="./images/images/tinto6.jpg">
+        <img src="../../src/routes/images/images/tinto6.jpg">
     </li> 
     <li>
-        <img src="./images/images/tinto7.jpg">
+        <img src="../../src/routes/images/images/tinto7.jpg">
     </li> 
     <li>
-        <img src="./images/images/tinto8.jpg">
+        <img src="../../src/routes/images/images/tinto8.jpg">
     </li> 
     <li>
-        <img src="./images/images/tinto9.jpg">
+        <img src="../../src/routes/images/images/tinto9.jpg">
     </li> 
     <li>
-        <img src="./images/images/tinto10.jpg">
+        <img src="../../src/routes/images/images/tinto10.jpg">
     </li> 
     <li>
-        <img src="./images/images/tinto11.jpg">
+        <img src="../../src/routes/images/images/tinto11.jpg">
     </li> 
     <li>
-        <img src="./images/images/tinto12.jpg">
+        <img src="../../src/routes/images/images/tinto12.jpg">
     </li> 
   </ul>
 

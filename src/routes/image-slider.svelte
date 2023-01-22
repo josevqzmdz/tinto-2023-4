1497f253f21b5cc16a6d251ef79fddd2482fdc76
josevqzmdz <notanothergenericaddress@gmail.com>
2023-01-21 21:55:57 -0600

    these css changes are the good ones

--- src/routes/image-slider.svelte
+++ src/routes/image-slider.svelte
@@ -6,50 +6,50 @@
         integrity="sha256-nQLuAZGRRcILA+6dMBOvcRh5Pe310sBpanc6+QBmyVM="
         crossorigin="anonymous">
     </script>
-    <link type="text/css" rel="stylesheet" href="..\..\..\latintomendez.com\src\routes\css\lightslider.css" />                  
-    <script src="..\..\..\latintomendez.com\src\routes\js\lightslider.js"></script>        
+    <link type="text/css" rel="stylesheet" href="..\..\latintomendez.com\static\css\lightslider.css" />                  
+    <script src="..\..\latintomendez.com\src\routes\js\lightslider.js"></script>        
 </head>
 
 <body>
 <ul id="lightSlider">
     <li>
-        <img src="..\..\latintomendez.com\src\routes\images\images\tinto.jpg">
+        <img src="..\..\latintomendez.com\static\images\images\tinto.jpg">
     </li>
     <li>
-        <img src="..\..\latintomendez.com\src\routes\images\images\pic1tinto.jpg">
+        <img src="..\..\latintomendez.com\static\images\images\pic1tinto.jpg">
     </li> 
     <li>
-        <img src="..\..\latintomendez.com\src\routes\images\images\pic2tinto.jpg">
+        <img src="..\..\latintomendez.com\static\images\images\pic2tinto.jpg">
     </li>    
     <li>
-        <img src="..\..\latintomendez.com\src\routes\images\images\pic3tinto.jpg">
+        <img src="..\..\latintomendez.com\static\images\images\pic3tinto.jpg">
     </li>     
     <li>
-        <img src="..\..\latintomendez.com\src\routes\images\images\pic4tinto.jpg">
+        <img src="..\..\latintomendez.com\static\images\images\pic4tinto.jpg">
     </li>      
     <li>
-        <img src="..\..\latintomendez.com\src\routes\images\images\pic5tinto.jpg">
+        <img src="..\..\latintomendez.com\static\images\images\pic5tinto.jpg">
     </li> 
     <li>
-        <img src="..\..\latintomendez.com\src\routes\images\images\tinto6.jpg">
+        <img src="..\..\latintomendez.com\static\images\images\tinto6.jpg">
     </li> 
     <li>
-        <img src="..\..\latintomendez.com\src\routes\images\images\tinto7.jpg">
+        <img src="..\..\latintomendez.com\static\images\images\tinto7.jpg">
     </li> 
     <li>
-        <img src="..\..\latintomendez.com\src\routes\images\images\tinto8.jpg">
+        <img src="..\..\latintomendez.com\static\images\images\tinto8.jpg">
     </li> 
     <li>
-        <img src="..\..\latintomendez.com\src\routes\images\images\tinto9.jpg">
+        <img src="..\..\latintomendez.com\static\images\images\tinto9.jpg">
     </li> 
     <li>
-        <img src="..\..\latintomendez.com\src\routes\images\images\tinto10.jpg">
+        <img src="..\..\latintomendez.com\static\images\images\tinto10.jpg">
     </li> 
     <li>
-        <img src="..\..\latintomendez.com\src\routes\images\images\tinto11.jpg">
+        <img src="..\..\latintomendez.com\static\images\images\tinto11.jpg">
     </li> 
     <li>
-        <img src="..\..\latintomendez.com\src\routes\images\images\tinto12.jpg">
+        <img src="..\..\latintomendez.com\static\images\images\tinto12.jpg">
     </li> 
   </ul>
 

cc006eaf957132e9fb84d51d46e80596d60102d0
josevqzmdz <notanothergenericaddress@gmail.com>
2023-01-23 11:40:02 -0600

    CSS finally works... part 3

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -7,7 +7,7 @@
 <!-- svelte-ignore a11y-missing-attribute -->
 <script>
     import Navbar from "./Sidebar.svelte";
-    import ImageSlider from "../../src/routes/Image-slider.svelte";
+    import ImageSlider from "../../src/routes/image-slider.svelte";
 </script>
 <html>
     <head>
